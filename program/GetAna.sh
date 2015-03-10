73dc4876bb6d9def082120a750850c391ee6630f
wuchen <wuchen1106@gmail.com>
2015-03-11 02:07:33 +0900

    update

--- program/GetAna.sh
+++ program/GetAna.sh
@@ -4,7 +4,7 @@ for (( i=0; i<270; i++ ))
 do
   if [ -f ../root/ana.$i.0309.0.root ]
   then
-    result=`./getAna $i 1 0309 0 100 7`
+    result=`./getAna $i 1 0309 0 0 7`
     echo $i" "$result
   fi
 done
